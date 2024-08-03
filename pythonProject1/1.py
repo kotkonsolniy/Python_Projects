b1d061fc796661175b62d9b1f4fb8c7e12795e20
kotkonsolniy <kotkonsolniy@gmail.com>
2024-08-03 19:18:01 +0300

    turtle

--- pythonProject1/1.py
+++ pythonProject1/1.py
@@ -1,12 +1,7 @@
 import turtle
-
-turtle.fd(100)
-turtle.left(90)
-turtle.fd(100)
-turtle.left(90)
-turtle.fd(100)
-turtle.rt(90)
-turtle.fd(100)
-turtle.rt(90)
-turtle.fd(100)
+i = 4
+while i > 0:
+    turtle.fd(100)
+    turtle.lt(90)
+    i = i - 1
 turtle.exitonclick()
